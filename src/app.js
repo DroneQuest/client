a0cd556f45c55f844188261715ede19bd017aaad
Kevin Sulonen <kevin.sulonen@gmail.com>
2016-05-02 16:34:16 -0700

    Change to WebSockets

--- src/app.js
+++ src/app.js
@@ -3,10 +3,20 @@
 const messages = require('./js/messages');
 var io = require('socket.io-client');
 
+// $(document).on('keydown', (event) => {
+//   var socket = io();
+//   console.log(event.keyCode);
+//   console.log(messages[event.keyCode]);
+//   socket.emit('keydown', messages[event.keyCode]);
+//   return false;
+// });
+
+
+var exampleSocket = new WebSocket('ws://127.0.0.1:3000');
+
 $(document).on('keydown', (event) => {
-  var socket = io();
-  console.log(event.keyCode);
+  exampleSocket.send("Hello Python!");
+  exampleSocket.send(messages[event.keyCode]);
   console.log(messages[event.keyCode]);
-  socket.emit('keydown', messages[event.keyCode]);
   return false;
 });
