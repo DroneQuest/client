fe1565297325a0b98f729732492920592a352815
YOUR FULL NAME <Ho.luc13@gmail.com>
2016-05-03 10:01:10 -0700

    drone flys, whats up!

--- src/app.js
+++ src/app.js
@@ -4,39 +4,48 @@ const angular = require('angular');
 var app = angular.module('app', []);
 
 app.controller('DroneController', ['$http', function($http){
-  var route = 'http://localhost:/8080/do/';
-  var command = null;
   var vm = this;
+  var route = 'http://127.0.0.1:8080/do/';
+  vm.codes = {
+    80: 'p', // PING p
+    13: 'takeoff', // TAKE_OFF enter
+    32: 'land', // LAND space
+    65: 'move_left', // MOVE_LEFT a
+    68: 'move_right', // MOVE_RIGHT d
+    38: 'move_up', // MOVE_UP up
+    40: 'move_down', // MOVE_DOWN down
+    87: 'move_forward', // MOVE_FORWARD w
+    83: 'move_backward', // MOVE_BACKWARD s
+    37: 'turn_left', // TURN_LEFT left
+    39: 'turn_right', // TURN_RIGHT right
+    8:  'reset', // RESET bs
+    69: '0x0D', // CALIBRATE e
+    187:'increase_speed', // INCREASE SPEED +
+    189:'decrease_speed', // DECREASE SPEED -
+    27: 'halt', // TERMINATE esc
+    kk: 'hover'  // HOVER
+  };
+  vm.command = null;
 
   vm.getCommands = function() {
 
   };
 
-  vm.postCommands = function(path, data) {
-    $http.post(route + path, data)
+  vm.postCommands = function(path) {
+    $http.post(route + path)
       .then((res) => {
         console.log('post working res: ', res);
       });
   };
 
   vm.keyPress = function(e) {
-    command = null;
-    if(e.keyCode === 13) {
-      command = 'takeoff';
-      vm.postCommands(command, e.keyCode);
-    }
-    if(e.keycode === 76) {
-      command = 'land';
-      vm.postCommands(command, e.keyCode);
+    vm.command = null;
+    if(e.keyCode in vm.codes) {
+      console.log(e.keyCode);
+      vm.command = vm.codes[e.keyCode];
+      vm.postCommands(vm.command);
+    } else {
+      console.log('not a valid key input.');
     }
   };
 }]);
-// const messages = require('./js/messages');
-// var socket = new WebSocket('ws://localhost:3000', 'echo-protocol');
-//
-// $(document).on('keydown', (event) => {
-//   socket.send('Hello Python!');
-//   socket.send(messages[event.keyCode]);
-//   console.log(messages[event.keyCode]);
-//   return false;
-// });
