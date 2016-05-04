171e336fa92a26d8ea2bea353ec92b4e7f0f1f85
Kevin Sulonen <kevin.sulonen@gmail.com>
2016-05-04 15:19:59 -0700

    Style additions

--- dist/bundle.js
+++ dist/bundle.js
@@ -83,7 +83,7 @@
 	exports.push([module.id, "@import url(https://fonts.googleapis.com/css?family=Raleway:400,400italic,700italic,700);", ""]);
 
 	// module
-	exports.push([module.id, "body {\n  margin: 0;\n  padding: 0;\n  font-family: 'Raleway', sans-serif;\n  color: #fff;\n  background: #696969; \n}\n\nnav {\n  box-shadow: 0em 0.4em 0.6em 0.1em #111;\n}\n\nnav ul {\n    list-style-type: none;\n    margin: 0;\n    padding: 0;\n    overflow: hidden;\n    background-color: #333;\n}\n\nnav li {\n    float: left;\n}\n\nnav li a {\n    display: block;\n    color: white;\n    text-align: center;\n    padding: 14px 16px;\n    text-decoration: none;\n}\n\nnav a:hover:not(.active) {\n    background-color: #111;\n}\n\nnav .active {\n  background-color:#4CAF50;\n}\n", ""]);
+	exports.push([module.id, "body {\n  margin: 0;\n  padding: 0;\n  font-family: 'Raleway', sans-serif;\n  color: #fff;\n  background: #696969; \n}\n\nnav {\n  font-weight: 700;\n  box-shadow: 0em 0.4em 0.6em 0.1em #111;\n}\n\nnav ul {\n  list-style-type: none;\n  margin: 0;\n  padding: 0;\n  overflow: hidden;\n  background-color: #333;\n}\n\nnav li {\n  float: left;\n}\n\nnav li a {\n  display: block;\n  color: white;\n  text-align: center;\n  padding: 14px 16px;\n  text-decoration: none;\n}\n\nnav a:hover {\n  background-color: #111;\n}\n\n.active {\n  background-color:#000;\n}\n\n.logo {\n  font-style: italic;\n  font-size: 1.8em;\n  display: inline-block;\n  float: right;\n  margin-top: -1.4em;\n  margin-right: 6em;\n}\n\nmain {\n  width: 90%;\n  margin: 3em auto;\n}\n\n.flight-control {\n  margin: auto;\n  width: 100%;\n  display: flex;\n  height: 500px;\n}\n\n.video {\n  border-radius: 5px;\n  height: 500px;\n  width: 75%;\n}\n\n.video img {\n  height: 100%;\n  width: 100%;\n}\n\n.status {\n  box-sizing: border-box;\n  border-radius: 5px;\n  height: 500px;\n  width: 24%;\n  border: 3px solid #333;\n  margin-left: 1%;\n}\n", ""]);
 
 	// exports
 
@@ -477,15 +477,17 @@
 	  }])
 
 	  .controller('PanelController', ['$location', function($location) {
-	    this.tab = '/fly';
-	    this.isActive = function(sometab) {
-	      this.tab = sometab;
+	    var vm = this;
+	    vm.tab = '/fly';
+	    vm.isActive = function(sometab) {
+	      if (vm.tab == sometab) return true;
 	    };
-	    this.setTab = function(newtab) {
-	      this.tab = newtab;
-	      $location.path('/' + this.tab);
+	    vm.setTab = function(newtab) {
+	      vm.tab = newtab;
+	      $location.path(vm.tab);
 	    };
 	  }])
+
 	  .directive('panelDirective', function() {
 	    return {
 	      restrict: 'E',
@@ -494,8 +496,14 @@
 	      templateUrl: '/templates/nav.html'
 	    };
 	  })
+
 	  .config(['$routeProvider', function(router) {
 	    router
+	      .when('/', {
+	        controller: 'DroneController',
+	        controllerAs: 'dronectrl',
+	        templateUrl: '/templates/fly-template.html'
+	      })
 	      .when('/fly', {
 	        controller: 'DroneController',
 	        controllerAs: 'dronectrl',
