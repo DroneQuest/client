8fd7d7eea3253aa2e45857bc9eec061524102d0f
YOUR FULL NAME <Ho.luc13@gmail.com>
2016-05-05 13:13:51 -0700

    test#7 rebuild

--- test/test_bundle.js
+++ test/test_bundle.js
@@ -270,7 +270,7 @@
 	    };
 
 	    vm.getImg = function() {
-	      $http.get('http://127.0.0.1:3000/imgdata')
+	      $http.get('http://127.0.0.1:8081/getvideo')
 	        .then((res) => {
 	          console.log('GET server img res: ', res);
 	        }, err => console.log('GET error: ', err));
